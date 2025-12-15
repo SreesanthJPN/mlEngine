ea3a1677e02028be3e76a757866ab852cf15ac8b
sreesanth <sreesanthjpn@gmail.com>
2025-12-15 23:45:11 +0530

    implemented global taskQueue

--- include/2dTiles.h
+++ include/2dTiles.h
@@ -1,3 +1,6 @@
+#ifndef _2DTILES_H
+#define _2DTILES_H
+
 typedef struct tileData{
 int rowStart, rowEnd, colStart, colEnd;
 }tileData;
@@ -10,3 +13,5 @@ typedef struct tile{
 tile* createTiles(int rows, int cols);
 
 tile* tileByRows(int rows, int cols, int tCols);
+
+#endif
